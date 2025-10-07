c4bb264da18c3d9f6c29027c06c40771ca1f2f29
Samuel Quintero <samuquinte777@gmail.com>
2025-10-07 18:26:24 -0500

    Add files via upload

--- Laboratorio/For/For.ino
+++ Laboratorio/For/For.ino
@@ -1,24 +1,18 @@
-const int ledPin =2;
-const int numBlinks = 5;
-
-
+const int ledPin = 2;
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(ledPin, OUTPUT);
-
-
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  for (int i=0;i<numBlinks; i++){
-    digitalWrite(ledPin, HIGH);
-    delay(1000);
-    digitalWrite(ledPin, LOW);
-    delay(1000);
 
+  for (int i = 0; i < 5; i++){
+    digitalWrite(2, HIGH);
+    delay(500);
+    digitalWrite(2, LOW);
+    delay(500);
   }
-  delay(5000);
 
+  delay(2000);
+  
 }
