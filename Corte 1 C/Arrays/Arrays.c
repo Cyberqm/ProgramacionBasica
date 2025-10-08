6e157c6d1be825b44bde402061ad21c9e7be71cd
Samuel Quintero <samuquinte777@gmail.com>
2025-10-07 19:23:51 -0500

    Add files via upload

---
+++ Corte 1 C/Arrays/Arrays.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+	int myNumbers[] = {25, 50, 75, 100};
+	
+	printf("%d\n", myNumbers[0]);
+	
+	int myNumber[] = {25, 20, 75, 100};
+	
+	myNumber[0]= 33;
+		
+		printf("%d\n", myNumber[0]);
+		
+	int myNumb[4];
+	
+	myNumb[0] = 25;
+	myNumb[1] = 50;
+	myNumb[2] = 75;
+	myNumb[3] = 100;
+	
+	printf("%d\n", myNumb[0]);
+	
+	
+	return 0;
+}
