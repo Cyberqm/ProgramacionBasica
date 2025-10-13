93f1eab4d671ce4c8cdc6fbc28ea8358c7f13e7a
Samuel Quintero <samuquinte777@gmail.com>
2025-10-13 15:27:34 -0500

    Add files via upload

---
+++ Corte 1 C/Proyecto_9/Proyecto_9.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+	int Num = 0;
+	do{
+		
+		printf("Value of Num = %d\n", Num);
+		
+		Num++;
+		
+	} while (Num<10);
+	
+	
+	return 0;
+}
