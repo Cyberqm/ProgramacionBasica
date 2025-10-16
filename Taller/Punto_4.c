d9c2fd03758fb33f42a9307a61dfd192f4177fe3
Samuel Quintero <samuquinte777@gmail.com>
2025-10-16 18:17:55 -0500

    Punto_4.c

--- Taller/Punto_4.c
+++ Taller/Punto_4.c
@@ -6,27 +6,28 @@ int main() {
 	float num1, num2;
 	
 	printf("Ingrese el primer numero: ");
-	scanf(" %f", &num1);
+	scanf("%f", &num1);
 	
 	printf("Ingrese el segundo numero: ");
-	scanf(" %f", &num2);
+	scanf("%f", &num2);
 	
 	if (num1>num2){
 		
-		printf("\n El numero mayor es: %.2f\n", num1);
+		printf("\nEl numero mayor es: %.2f\n", num1);
 		
 	}
 	
 	else if (num1<num2){
 		
-		printf("\n El numero mayor es: %.2f\n", num2);
+		printf("\nEl numero mayor es: %.2f\n", num2);
 		
 	}
 	
 	else {
 		
-		printf("\n Los 2 numeros son iguales.\n");
+		printf("\nLos 2 numeros son iguales.\n");
 	}
 	
 	return 0;
+
 }
